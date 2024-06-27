46b48e5d7b94b6d161a8f27acda2597c42a07a7b
Sneha Reddy <snehareddy9178@gmail.com>
2024-06-27 10:23:18 +0530

    connect to db

--- order-service/src/index.ts
+++ order-service/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL ?? '')
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((err) => {
+    console.log(`Mongo Connection Failed ${err}`);
+  });
 
 const app = express();
 
